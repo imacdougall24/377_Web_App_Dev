7f3e5c60de7e83ee4b5506e9161fe229ce9e8a55
imacdougall24 <imacdougall24@hanoverstudents.org>
2023-10-19 10:49:29 -0400

    Saving this game (craps)

--- GroupSiteGame/script.js
+++ GroupSiteGame/script.js
@@ -1,24 +1,48 @@
-function rollDie(){
+/*
+* Sets an initial value of the die to display a random side
+*/
+$(document).ready(function (){
+    rollDice();
+})
+/*
+* Rolls the die to show a random number between 1 and 6
+*/
+function rollDie(dieNum){
     var roll = Math.floor(Math.random() * 6 + 1);
-    $(".pip").css("visibility", "hidden")
 
     if (roll == 1){
-        $("#d1p4").css("visibility", "visible")
+        $("#d" + dieNum + "p4").css("visibility", "visible")
     }
     else if (roll == 2){
-        $("#d1p1, #d1p7").css("visibility", "visible")
+        $("#d" + dieNum + "p1, #d" + dieNum + "p7").css("visibility", "visible")
     }
     else if (roll == 3){
-        $("#d1p3, #d1p4, #d1p5").css("visibility", "visible")
+        $("#d" + dieNum + "p3, #d" + dieNum + "p4, #d" + dieNum + "p5").css("visibility", "visible")
     }
     else if (roll == 4){
-        $("#d1p1, #d1p3, #d1p5, #d1p7").css("visibility", "visible")
+        $("#d" + dieNum + "p1, #d" + dieNum + "p3, #d" + dieNum + "p5, #d" + dieNum + "p7").css("visibility", "visible")
     }
     else if (roll == 5){
-        $("#d1p1, #d1p3, #d1p4, #d1p5, #d1p7").css("visibility", "visible")
+        $("#d" + dieNum + "p1, #d" + dieNum + "p3, #d" + dieNum + "p4, #d" + dieNum + "p5, #d" + dieNum + "p7").css("visibility", "visible")
     }
     else if (roll == 6){
-        $("#d1p1, #d1p2, #d1p3, #d1p5, #d1p6, #d1p7").css("visibility", "visible")
+        $("#d" + dieNum + "p1, #d" + dieNum + "p2, #d" + dieNum + "p3, #d" + dieNum + "p5, #d" + dieNum + "p6, #d" + dieNum + "p7").css("visibility", "visible")
     }
+    return roll;
+
+}
+
+/* Rolls dice */
+function rollDice(){
+    // Clears the pips
+    $(".pip").css("visibility", "hidden")
+
+    // Roll the two die
+    var roll1 = rollDie(1);
+    var roll2 = rollDie(2);
+
+    // Gets total
+    var total = roll1 + roll2;
+    console.log("Total: " + total);
 
 }
